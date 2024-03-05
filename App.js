a9cd0b8045a96872175dfbcc53d3f287a4e10403
FrKaua <kaua.felipe1b3@gmail.com>
2024-03-05 11:44:10 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Kauã Felipe Rego</Text>
+      <Text>05/03/2024 DDM 3B3 lado B</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
